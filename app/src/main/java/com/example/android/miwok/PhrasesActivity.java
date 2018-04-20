2a21f684b41802008f8777aa4b0acd022515db7b
Simon <mendellovejazz@outlook.com>
2018-04-20 19:39:28 +0800

    Release Media add in PhraseActivity

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.miwok;
 
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
@@ -14,10 +16,39 @@ import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
 
+    private MediaPlayer mMediaPlayer;
+
+    private AudioManager mAudioManager;
+
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            }else if(focusChange == AudioManager.AUDIOFOCUS_LOSS)
+                releaseMediaPlayer();
+            else  if(focusChange == AudioManager.AUDIOFOCUS_GAIN)
+                mMediaPlayer.start();
+        }
+    };
+
+    private MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrases);
+        mAudioManager = (AudioManager)this.getSystemService(this.AUDIO_SERVICE);
+
+
         final ArrayList<Word> words = new ArrayList<>();
         words.add(new Word("Where are you going?","minto wuksus",R.raw.phrase_where_are_you_going));
         words.add(new Word("What is your name?","tinnə oyaase'nə",R.raw.phrase_what_is_your_name));
@@ -38,23 +69,44 @@ public class PhrasesActivity extends AppCompatActivity {
             @TargetApi(Build.VERSION_CODES.M)
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                releaseMediaPlayer();
                 Word word = words.get(adapterView.getPositionForView(view));
-                final MediaPlayer mMediaPlayer = MediaPlayer.create(PhrasesActivity.this,word.getMediaResourceID());
-                mMediaPlayer.start();
-//                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
-//                    public void onCompletion(MediaPlayer mediaPlayer){
-//                        Toast.makeText(PhrasesActivity.this,"End",Toast.LENGTH_SHORT).show();
-//                        if(mMediaPlayer != null)
-//                            mMediaPlayer.release();
-//                    }
-//
-//                });
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(result==AudioManager.AUDIOFOCUS_REQUEST_GRANTED){
+                    mMediaPlayer = MediaPlayer.create(PhrasesActivity.this,word.getMediaResourceID());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                }
+
+
             }
         });
 
     }
 
-//    private void releaseMediaPlayer(MediaPlayer){
-//        if()
-//    }
+    public void onStop(){
+        super.onStop();
+        releaseMediaPlayer();
+    }
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, abandon it. This also
+            // unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
+    }
 }
